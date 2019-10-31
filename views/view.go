3af43a2880d7716186a6766a6e7c34365a8656c5
Shou Tseng <yakushou730@gmail.com>
2019-11-01 00:44:58 +0800

    Chapter 6.3.3

--- views/view.go
+++ views/view.go
@@ -4,15 +4,17 @@ import "html/template"
 
 type View struct {
 	Template *template.Template
+	Layout   string
 }
 
-func NewView(files ...string) *View {
-	files = append(files, "views/layouts/footer.gohtml")
+func NewView(layout string, files ...string) *View {
+	files = append(files, "views/layouts/footer.gohtml", "views/layouts/bootstrap.gohtml")
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 	return &View{
 		Template: t,
+		Layout:   layout,
 	}
 }
