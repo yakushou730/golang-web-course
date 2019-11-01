da9ebcc4dea3c59740bdd13e3a25225dc5d134d8
Shou Tseng <yakushou730@gmail.com>
2019-11-01 16:43:39 +0800

    Chapter 7.1.1

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ var (
 	homeView        *views.View
 	contactView     *views.View
 	faqView         *views.View
+	signupView      *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,11 @@ func faq(w http.ResponseWriter, r *http.Request) {
 	must(faqView.Render(w, nil))
 }
 
+func signup(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signupView.Render(w, nil))
+}
+
 // A helper function that panics on any error
 func must(err error) {
 	if err != nil {
@@ -43,11 +49,13 @@ func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
 	faqView = views.NewView("bootstrap_v2", "views/faq.gohtml")
+	signupView = views.NewView("bootstrap", "views/signup.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
 	r.HandleFunc("/faq", faq)
+	r.HandleFunc("/signup", signup)
 
 	http.ListenAndServe(":3000", r)
 }
