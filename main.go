b9990859f527e10299b6588e9c685f60a408e4b6
Shou Tseng <yakushou730@gmail.com>
2019-11-01 15:44:58 +0800

    Exercise 6.6.1 6.6.2

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ var (
 	contactTemplate *template.Template
 	homeView        *views.View
 	contactView     *views.View
+	faqView         *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -26,6 +27,11 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(faqView.Render(w, nil))
+}
+
 // A helper function that panics on any error
 func must(err error) {
 	if err != nil {
@@ -36,10 +42,12 @@ func must(err error) {
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqView = views.NewView("bootstrap", "views/faq.gohtml")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 
 	http.ListenAndServe(":3000", r)
 }
