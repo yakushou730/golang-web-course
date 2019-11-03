f93dccc02b4976a644bee51e0ddf1b4ff7d89962
Shou Tseng <yakushou730@gmail.com>
2019-11-03 15:41:32 +0800

    Chapter 7.3.2

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,23 @@
 package controllers
 
-type Users struct{}
+import (
+	"net/http"
+
+	"github.com/yakushou730/golang-web-course/views"
+)
+
+type Users struct {
+	NewView *views.View
+}
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap", "views/users/new.gohtml"),
+	}
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
