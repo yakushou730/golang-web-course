700545d057ec902e9a009a2c57fee2757936585b
Shou Tseng <yakushou730@gmail.com>
2019-11-03 23:42:18 +0800

    Chapter 7.4.5

--- controllers/users.go
+++ controllers/users.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"github.com/yakushou730/golang-web-course/views"
 )
 
@@ -30,14 +29,10 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
+	var form SignupForm
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
-	dec := schema.NewDecoder()
-	form := SignupForm{}
-	if err := dec.Decode(&form, r.PostForm); err != nil {
-		panic(err)
-	}
-
-	fmt.Fprintln(w, form)
+	fmt.Fprintln(w, "Email is", form.Email)
+	fmt.Fprintln(w, "Password is", form.Password)
 }
