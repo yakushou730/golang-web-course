3d85ea9773b2c33fe2ce2b39a9b71f77ef344df7
Shou Tseng <yakushou730@gmail.com>
2019-11-03 15:57:54 +0800

    Chapter 7.4.1

--- controllers/users.go
+++ controllers/users.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/yakushou730/golang-web-course/views"
@@ -20,4 +21,8 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
 	}
+}
+
+func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "This is a temporary response.")
 }
