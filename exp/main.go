3f2522bc1798a5c4909bf1a746cd1f0c6699073d
Shou Tseng <yakushou730@gmail.com>
2019-11-06 18:43:19 +0800

    Chapter 8.3.1

--- exp/main.go
+++ exp/main.go
@@ -1,37 +1,32 @@
 package main
 
 import (
-	"html/template"
-	"os"
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "TsengYaoShang"
+	dbname = "golang_test"
 )
 
 func main() {
-	t, err := template.ParseFiles("hello.gohtml")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"dbname=%s sslmode=disable",
+		host, port, user, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
-	data := struct {
-		Name      string
-		Email     string
-		Number    int32
-		Decimal   float32
-		TestMap   map[string]int32
-		Condition bool
-	}{
-		Name:    "<script>alert('Howdy!');</script>",
-		Email:   "yakushou730@gmail.com",
-		Number:  10,
-		Decimal: 123.456,
-		TestMap: map[string]int32{
-			"Test1": 123,
-			"Test2": 456,
-		},
-		Condition: false,
-	}
-
-	err = t.Execute(os.Stdout, data)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Successfully connected!")
+	db.Close()
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.7.3
 	github.com/gorilla/schema v1.1.0
 	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.2.0
 )
