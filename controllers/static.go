21282287810f5e26f74abd6d1ffb798d9122d0cf
Shou Tseng <yakushou730@gmail.com>
2019-11-06 12:13:00 +0800

    Chapter 7.5.2

--- controllers/static.go
+++ controllers/static.go
@@ -10,8 +10,8 @@ type Static struct {
 
 func NewStatic() *Static {
 	return &Static{
-		Home:    views.NewView("bootstrap", "views/static/home.gohtml"),
-		Contact: views.NewView("bootstrap", "views/static/contact.gohtml"),
-		Faq:     views.NewView("bootstrap_v2", "views/static/faq.gohtml"),
+		Home:    views.NewView("bootstrap", "static/home"),
+		Contact: views.NewView("bootstrap", "static/contact"),
+		Faq:     views.NewView("bootstrap_v2", "static/faq"),
 	}
 }
--- views/view.go
+++ views/view.go
@@ -13,10 +13,13 @@ type View struct {
 
 var (
 	LayoutDir   string = "views/layouts/"
+	TemplateDir string = "views/"
 	TemplateExt string = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
+	addTemplatePath(files)
+	addTemplateExt(files)
 	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
@@ -44,5 +47,17 @@ func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if err := v.Render(w, nil); err != nil {
 		panic(err)
+	}
+}
+
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+func addTemplateExt(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExt
 	}
 }
