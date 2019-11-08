bfbe38a8657031dd6e24c46799df1adef5bc6a3f
Shou Tseng <yakushou730@gmail.com>
2019-11-08 09:46:37 +0800

    Chapter 8.4.8

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 
 	"github.com/jinzhu/gorm"
 
@@ -12,16 +9,24 @@ import (
 )
 
 const (
-	host   = "localhost"
-	port   = 5432
-	user   = "TsengYaoShang"
-	dbname = "golang_test"
+	host     = "localhost"
+	port     = 5432
+	user     = "TsengYaoShang"
+	password = ""
+	dbname   = "golang_test"
 )
 
 type User struct {
 	gorm.Model
 	Name  string
 	Email string `gorm:"not null;unique_index"`
+}
+
+type Order struct {
+	gorm.Model
+	UserId      uint
+	Amount      int
+	Description string
 }
 
 func main() {
@@ -33,28 +38,28 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
+
 	db.LogMode(true)
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Order{})
 
-	name, email := getInfo()
+	var user User
+	db.First(&user)
+	if db.Error != nil {
+		panic(db.Error)
+	}
 
-	u := &User{
-		Name:  name,
-		Email: email,
-	}
-	if err = db.Create(u).Error; err != nil {
-		panic(err)
-	}
-	fmt.Printf("%+v\n", u)
+	createOrder(db, user, 1001, "Fake Description #1")
+	createOrder(db, user, 9999, "Fake Description #2")
+	createOrder(db, user, 8800, "Fake Description #3")
 }
 
-func getInfo() (name, email string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name?")
-	name, _ = reader.ReadString('\n')
-	name = strings.TrimSpace(name)
-	fmt.Println("What is your email?")
-	email, _ = reader.ReadString('\n')
-	email = strings.TrimSpace(email)
-	return name, email
+func createOrder(db *gorm.DB, user User, amount int, desc string) {
+	db.Create(&Order{
+		UserId:      user.ID,
+		Amount:      amount,
+		Description: desc,
+	})
+	if db.Error != nil {
+		panic(db.Error)
+	}
 }
