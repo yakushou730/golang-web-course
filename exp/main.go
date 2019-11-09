778a6e138a8f97dbc38b3092ec6185068a3fd0f9
Shou Tseng <yakushou730@gmail.com>
2019-11-09 13:58:52 +0800

    Chapter 9.2

--- exp/main.go
+++ exp/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/jinzhu/gorm"
+	"github.com/yakushou730/golang-web-course/models"
 
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -16,40 +16,23 @@ const (
 	dbname   = "golang_test"
 )
 
-type User struct {
-	gorm.Model
-	Name   string
-	Email  string `gorm:"not null;unique_index"`
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserId      uint
-	Amount      int
-	Description string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"dbname=%s sslmode=disable",
 		host, port, user, dbname)
-	db, err := gorm.Open("postgres", psqlInfo)
+	us, err := models.NewUserService(psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer us.Close()
+	us.DestructiveReset()
 
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
-
-	var user User
-	db.Preload("Orders").First(&user)
-	if db.Error != nil {
-		panic(db.Error)
+	// This will error because you DO NOT have a user with
+	// this ID, but we will create on soon.
+	user, err := us.ByID(1)
+	if err != nil {
+		panic(err)
 	}
 
-	fmt.Println("Email:", user.Email)
-	fmt.Println("Number of orders:", len(user.Orders))
-	fmt.Println("Orders:", user.Orders)
+	fmt.Println(user)
 }
