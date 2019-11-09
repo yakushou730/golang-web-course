90d5d1a19b3796d65d38eb1b997ac367c6e18099
Shou Tseng <yakushou730@gmail.com>
2019-11-09 15:10:59 +0800

    Chapter 9.6

--- models/users.go
+++ models/users.go
@@ -76,9 +76,12 @@ func (us *UserService) ByEmail(email string) (*User, error) {
 }
 
 // DestructiveReset drops the user table and rebuilds it
-func (us *UserService) DestructiveReset() {
-	us.db.DropTableIfExists(&User{})
-	us.db.AutoMigrate(&User{})
+func (us *UserService) DestructiveReset() error {
+	err := us.db.DropTableIfExists(&User{}).Error
+	if err != nil {
+		return err
+	}
+	return us.AutoMigrate()
 }
 
 // Create will create the provided user and backfill data
@@ -111,4 +114,13 @@ func (us *UserService) Delete(id uint) error {
 	}
 	user := User{Model: gorm.Model{ID: id}}
 	return us.db.Delete(&user).Error
+}
+
+// AutoMigrate will attempt to automatically migrate the
+// users table
+func (us *UserService) AutoMigrate() error {
+	if err := us.db.AutoMigrate(&User{}).Error; err != nil {
+		return err
+	}
+	return nil
 }
