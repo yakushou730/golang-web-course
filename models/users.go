89100d8057e645d352e7fcc53239c204af7e4d95
Shou Tseng <yakushou730@gmail.com>
2019-11-11 18:31:46 +0800

    Chapter 10.4.1

--- models/users.go
+++ models/users.go
@@ -19,9 +19,11 @@ var (
 
 type User struct {
 	gorm.Model
-	Age   int
-	Name  string
-	Email string `gorm:"not null;unique_index"`
+	Age          int
+	Name         string
+	Email        string `gorm:"not null;unique_index"`
+	Password     string `gorm:"-"`
+	PasswordHash string `gorm:"not null"`
 }
 
 type UserService struct {
