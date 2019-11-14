6ef70b6aaf8009bfe92a566a038196cefb5fdbda
Shou Tseng <yakushou730@gmail.com>
2019-11-14 08:31:33 +0800

    Chapter 11.6.3

--- exp/main.go
+++ exp/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/yakushou730/golang-web-course/rand"
+	"github.com/yakushou730/golang-web-course/hash"
 )
 
 func main() {
-	fmt.Println(rand.String(10))
-	fmt.Println(rand.RememberToken())
+	hmac := hash.NewHMAC("my-secret-key")
+	// This should print out:
+	// 4waUFc1cnuxoM2oUOJfpGZLGP1asj35y7teuweSFgPY=
+	fmt.Println(hmac.Hash("this is my string to hash"))
 }
