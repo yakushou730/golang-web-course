f4f8c41b4c3fbf4785db69cd7d415b7b245e9ee1
Shou Tseng <yakushou730@gmail.com>
2020-01-28 22:13:45 +0800

    Chapter 15.6

--- models/galleries.go
+++ models/galleries.go
@@ -155,3 +155,28 @@ func (gg *galleryGorm) ByUserID(userID uint) ([]Gallery, error) {
 	}
 	return galleries, nil
 }
+
+func (g *Gallery) ImagesSplitN(n int) [][]string {
+	// Create out 2D slice
+	ret := make([][]string, n)
+	// Create the inner slices - we need N of them, and we will
+	// start them with a size of 0.
+	for i := 0; i < n; i++ {
+		ret[i] = make([]string, 0)
+	}
+	// Iterate over our images, using the index % n to determine
+	// which of the slices in ret to add the image to.
+	for i, img := range g.Images {
+		// % is the remainder operator in Go
+		// eg:
+		// 0%3 = 0
+		// 1%3 = 1
+		// 2%3 = 2
+		// 3%3 = 0
+		// 4%3 = 1
+		// 5%3 = 2
+		bucket := i % n
+		ret[bucket] = append(ret[bucket], img)
+	}
+	return ret
+}
